78534b16f70ea382a0aab6f71502723391bdf329
Martin Hoshi Vognsen <martinvognsen@gmail.com>
2020-09-27 15:41:13 +0200

    m

---
+++ Assignment 2.2_group2/r/2.2_functions.R
@@ -0,0 +1,57 @@
+## # Generate vector of simulated data for single sim path
+#x <- 
+
+## Sn
+# Sn <- 
+
+
+## g
+#g <- 
+
+
+## phi
+# phi <- 
+
+
+MCI_ruin <- function(n, m, a, b) {
+  # n is number of days the company earns/loses money
+  # m is the number withdrawings
+  # a and b are the upper and lower bound where the distribution takes place
+  numberOfDefaults <- 0
+  probability_of_default <- 0
+  
+  for(i in 1:m) {
+    startCapital <- 30
+    earnings <- runif(n, a, b)
+    for(j in seq_along(earnings)) {
+      startCapital <- startCapital + earnings[j]
+      if(startCapital <= 0) {
+        numberOfDefaults <- numberOfDefaults + 1
+        break
+      }
+    }
+  }
+  probabilityOfDefault <- numberOfDefaults / m
+  probabilityOfDefault
+}
+
+
+MCI_ruin_vectorized <- function(n, m, a, b) {
+  # n is number of days the company earns/loses money
+  # m is the number withdrawings
+  # a and b are the upper and lower bound where the distribution takes place
+  numberOfDefaults <- 0
+  probabilityOfDefault <- 0
+  
+  for(i in 1:m) {
+    startCapital <- 30
+    earnings <- runif(n, a, b)
+    capital <- startCapital + cumsum(earnings)
+    if( min(capital) <= 0) {
+      numberOfDefaults <- numberOfDefaults + 1
+      next
+    }
+  }
+  probabilityOfDefault <- numberOfDefaults / m
+  probabilityOfDefault
+}
---
+++ Assignment 2.2_group2/r/2.2_script.R
@@ -0,0 +1,63 @@
+## Assignment 2-2 ----
+## Importance sampling: Univariate Simulation ----
+
+## *** ----
+## SETUP ----
+
+{
+  rm(list=ls())
+  if(dev.cur() != 1){dev.off()}
+  
+  ## Libraries
+  library(microbenchmark)
+  library(ggplot2)
+  library(profvis)
+  library(Matrix)
+  library(reshape2)
+}
+
+## Constants
+
+m = 1000 ## Number of paths
+n = 100 ## Number of steps in path
+
+
+## *** ----
+## ESTIMATION ====
+
+
+## Sn ----
+
+
+## Monte Carlo Integral ----
+
+
+## Importance Sampling ----
+
+## **************************************
+
+## OOP ====  
+## CONSTRUCTORS ----
+
+
+
+## VALIDATORS ----
+
+
+
+## HELPERS ----
+
+
+
+
+## *** ----
+## PROFILING ----
+
+
+## *** ----
+## BENCHMARKING ----
+
+## Tests ----
+## 1) Runif ----
+
+## 2) Runtime wrt. n ----
