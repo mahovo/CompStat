78406539a0253e62d792c3ca30a1faa0d121c010
Martin Hoshi Vognsen <martinvognsen@gmail.com>
2020-10-27 16:06:13 +0100

    Working implementation with some plots

--- Assignment 3.2_group2/r/3.2_functions.R
+++ Assignment 3.2_group2/r/3.2_functions.R
@@ -1,214 +1,174 @@
-## EM ----
+
+## EM function factory ----
 ## https://cswr.nrhstat.org/8-1-basic-properties.html
-
-
-
-
-
-
-## EM
 {
   EM <- function(
     par,
-    x,
+    x_sim,
     d = 0.8, 
     c = 0.1, 
     gamma0 = 0.01,
-    maxit = 1000,
+    nu = 5,
+    maxit = 100,
+    maxit_gd = 100,
+    maxit_range_check = 20, ## Number of times to adjust step size while checking parameter ranges
     epsilon = 1e-6,
+    optimizer = gd,
+    eps_gd = 1e-16,
     cb = NULL) {
     
-    nu <- 5
-    n <- length(x)
-    const1 <- gamma((nu + 1)/2)/(sqrt(nu * pi) * gamma(nu/2))
-    
+    n <- length(x_sim)
+
+    ## t_dens() **********************************************
     ## t-distribution density function
+    const1 <- gamma((nu + 1)/2)/(sqrt(nu * pi) * gamma(nu/2))
+    const2 <- (nu + 1) / 2
     t_dens <- function(x_i, mu, sigma) {
       if(sigma <= 0)
         warning("sigma <= 0")
       (const1 * 
-          (1 + (x_i - mu)^2/(nu * sigma^2))^(-(nu + 1)/2)
+          (1 + (x_i - mu)^2/(nu * sigma^2))^(-const2)
       )/sigma
     }
     
-    ## Q
-    Q <- function(par, p_hat){
+    ## Q() ***************************************************
+    Q <- function(par, p_hat, xx){
       p <- par[1]
       mu1 <- par[2]
       mu2 <- par[3]
       sigma1 <- par[4]
       sigma2 <- par[5]
       nu <- nu1 <- nu2 <- 5
+      
+      ## Check that params are in range
       if(p <= 0)
-        warning("p <= 0")
-        #print("p <= 0")
+        warning("p <= 0 in Q()")
+      if(p < 0 || p > 1)
+        return(Inf)
       if(sigma1 <= 0 )
-        warning("sigma1 <= 0")
-        #print("sigma1 <= 0")
+        warning("sigma1 <= 0  in Q()")
       if(sigma2 <= 0 )
-        warning("sigma2 <= 0")
-        #print("sigma2 <= 0")
-      sum = 0
+        warning("sigma2 <= 0  in Q()")
       
-      sum(p_hat * (
-          log(p * t_dens(x, mu1, sigma1))
-          + (1 - p_hat) * (
-            log((1 - p) * t_dens(x, mu2, sigma2))
-          )
-        )
+      sum(p_hat * log(p * t_dens(xx, mu1, sigma1))
+          + (1 - p_hat) * log((1 - p) * t_dens(xx, mu2, sigma2))
       )
-      
-      
-      # for(i in seq_along(x)){
-      #   sum = sum + p_hat[i] * (
-      #     log(p * t_dens(x[i], mu1, sigma1))
-      #     + (1 - p_hat[i]) * (
-      #       log((1 - p) * t_dens(x[i], mu2, sigma2))
-      #     )
-      #   )
-      # }
-      # - sum ## Negative log-likelihood
     }
     
-    grad_Q <- function(par, p_hat){
+    ## grad_Q() ***********************************************
+    ## Gradient of Q
+    grad_Q <- function(par, p_hat, x){
       p <- par[1]
       mu1 <- par[2]
       mu2 <- par[3]
       sigma1 <- par[4]
       sigma2 <- par[5]
-      nu <- nu1 <- nu2 <- 5
-      
-      const2 <- (nu + 1) / 2
-      
+  
       dp <- sum((p_hat - p) /(p * (1 - p)))
       dmu1 <- sum((2 * p_hat * const2 * (x - mu1))/(nu * sigma1^2 + (x - mu1)^2))
       dmu2 <- sum((2 * (1 - p_hat) * const2 * (x - mu2))/(nu * sigma2^2 + (x - mu2)^2))
       dsigma1 <- sum(p_hat * (
         (
           2 * const2 * (x - mu1)^2) / (nu * sigma1^3 + sigma1 * (x - mu1)^2
-        )
+          )
         - (1/sigma1)
       ))
       dsigma2 <- sum((1 - p_hat) * (
         (
-          2 * const2 * (x - mu1)^2) / (nu * sigma1^3 + sigma1 * (x - mu1)^2
+          2 * const2 * (x - mu2)^2) / (nu * sigma2^3 + sigma2 * (x - mu2)^2
           )
-        - (1/sigma1)
+        - (1/sigma2)
       ))
       
       c(dp, dmu1, dmu2, dsigma1, dsigma2)
     }
     
+    ## GD() *********************************************************
+    ## Gradient descent
+    GD <- function (par, p_hat, eps_gd) {
+      H <- function(...) {-Q(...)}
+      grad_H <- function(...) {-grad_Q(...)}
+      for(i in 1:maxit_gd) {
+        it <- i
+        H_value <- H(par, p_hat, xx = x_sim)
+        if(!is.null(cb)) cb()
+        grad_H <- grad_H(par = par, p_hat = p_hat, x = x_sim)
+        #if(!is.null(cb)) cb()
+        h_prime <- sum(grad_H^2)
+        #if(!is.null(cb)) cb()
+        
+        ## Convergence criterion based on gradient norm
+        if(h_prime <= eps_gd) break
+        gamma <- gamma0
+        
+        ## Proposed descent step
+        par1 <- par - gamma * grad_H
+
+        ## Check that p is in range.
+        ## Else reduce stepsize until p is in range.
+        if(par1[1] <= 0 || par1[1] >= 1) {
+          for(i in 1:maxit_range_check) {
+            it = i
+            if (par1[1] <= 0 || par1[1] >= 1) {
+              warning("p out of range in GD()")
+              gamma = gamma/2
+              par1 <- par - gamma * grad_H
+            } else {
+              break
+            }
+            if(it == maxit_range_check ) warning("Range check of p exceeded max number of iterations.")
+          }
+        }
+ 
+        ## Backtracking while descent is insufficient
+        while(H(par1, p_hat, xx = x_sim) > H_value - c * gamma * h_prime) {
+          gamma <- d * gamma
+          par1 <- par - gamma * grad_H
+        }
+      }
+      if(it == maxit_gd)
+        warning("Number of iterations > maxit_gd = ", maxit_gd)
+      par <- par1
+    }
+    
+    ## E_step() **********************************************
     ## Calculate conditional expectations
-    EStep <- function(par) {
+    E_step <- function(par) {
       mu1 <- par[2]
       mu2 <- par[3]
       sigma1 <- par[4]
       sigma2 <- par[5]
       a <- numeric(n)
       b <- numeric(n)
-      # for(i in seq_along(x)) {
-      #   a[i] <- p * t_dens(x[i], mu1, sigma1)
-      #   b[i] <- (1 - p)* t_dens(x[i], mu2, sigma2)
-      # }
-      a <- p * t_dens(x, mu1, sigma1)
-      b <- (1 - p) * t_dens(x, mu2, sigma2)
-      #print(paste("EStep sigma1 = ", sigma1))
-      #print(paste("EStep sigma2 = ", sigma2))
+      a <- p * t_dens(x_sim, mu1, sigma1)
+      b <- (1 - p) * t_dens(x_sim, mu2, sigma2)
       a / (a + b)
     }
+    
+    ## M_step() **********************************************
     ## Maximize Q
-    MStep <- function(par, p_hat) {
-      # N1 <- sum(p_hat)
-      # N2 <- n - N1
-      # c(N1 / n, sum(p_hat * x) / N1, sum((1 - p_hat) * x) / N2, 1, 2) ## Fixed sigma. 
-      
-      H_value <- -Q(par, p_hat)
-      #print(paste("MStep value = ", H_value))
-      grad_H <- - grad_Q(par, p_hat)
-      #print(paste("min(grad) = ", min(grad_H)))
-      #print(paste("max(grad) = ", max(grad_H)))
-      h_prime <- sum(grad_H^2)
-      #print(paste("h_prime = ", h_prime))
-      ## Convergence criterion based on gradient norm
-      if(h_prime <= epsilon) break
-      gamma <- gamma0
-      ## Proposed descent step
-      par1 <- par - gamma * grad_H
-      if (par1[1] <= 0 || par1[1] >= 1) {
-        warning("p out of range")
-        #print("p out of range")
-      }
-      if (par1[4] <= 0) {
-        warning("sigma1 out of range")
-      }
-      if (par1[5] <= 0) {
-        warning("sigma2 out of range")
-      }
-      #print(paste("par1 = ", par1))
-      ## Backtracking while descent is insufficient
-      while(Q(par1, p_hat) > H_value - c * gamma * h_prime) {
-        gamma <- d * gamma
-        par1 <- par - gamma * grad_H
-      }
-      par <- par1
+    M_step <- function(par, p_hat) {
+      switch(
+        optimizer,
+        gd = GD(par, p_hat, eps_gd),
+        sg = SG(par, p_hat, gamma = 1e-5, num_epochs = 40)
+      )
     }
     for(i in 1:maxit) {
+      it <- i
       par0 <- par
-      p_hat <- EStep(par)
-      par <- MStep(par, p_hat)
-      diff <- par - par0
+      p_hat <- E_step(par)
+      par <- M_step(par, p_hat)
       sum_sq_diff <- sum((par - par0)^2)
       if(!is.null(cb)) cb()
       if(sum_sq_diff <= epsilon * (sum(par^2) + epsilon))
         break
     }
-    print(paste("Number of iterations: ", i))
-    if(i == maxit)
-      warning("Maximal number, ", maxit, ", of iterations reached")
+    print(paste("Number of iterations: ", it))
+    if(it == maxit)
+      warning("Number of iterations > maxit = ", maxit)
     par
   }
 }
-
-### Extracting function from EM algorithm
-EStep <- environment(EM)$EStep ## E step
-MStep <- environment(EM)$MStep ## M step
-
-
-
-## Gradient Descent ----
-## https://cswr.nrhstat.org/7-2-descent-direction-algorithms.html
-# GD <- function(
-#   par, 
-#   H,
-#   gradient,
-#   d = 0.8, 
-#   c = 0.1, 
-#   gamma0 = 0.01, 
-#   epsilon = 1e-4, 
-#   maxit = 1000,
-#   cb = NULL
-# ) {
-#   for(i in 1:maxit) {
-#     value <- H(par)
-#     grad <- gradient(par)
-#     h_prime <- sum(grad^2)
-#     if(!is.null(cb)) cb()
-#     # Convergence criterion based on gradient norm
-#     if(h_prime <= epsilon) break
-#     gamma <- gamma0
-#     # Proposed descent step
-#     par1 <- par - gamma * grad
-#     # Backtracking while descent is insufficient
-#     while(H(par1) > value - c * gamma * h_prime) {
-#       gamma <- d * gamma
-#       par1 <- par - gamma * grad
-#     }
-#     par <- par1
-#   }
-#   if(i == maxit)
-#     warning("Maximal number, ", maxit, ", of iterations reached")
-#   par
-# }
 
 
--- Assignment 3.2_group2/r/3.2_script.R
+++ Assignment 3.2_group2/r/3.2_script.R
@@ -23,16 +23,18 @@
   #install_github("nielsrhansen/CSwR/CSwR_package")
   library(CSwR)
   
+  library(plotly)
+  
   # Source functions
-  #source("/Users/mhvpbp13/Library/Mobile Documents/com~apple~CloudDocs/CBS/cbs/Semester k1/CompStat/2020/Assignments/git/CompStat/Assignment 3.2_group2/r/3.2_functions.R")
+  source("/Users/mhvpbp13/Library/Mobile Documents/com~apple~CloudDocs/CBS/cbs/Semester k1/CompStat/2020/Assignments/git/CompStat/Assignment 3.2_group2/r/3.2_functions.R")
 }
 
 
 # *** ----
 ## DATA ----
-## Simulate data ----
+## > Simulate data ----
 
-## Sample from t-distribution
+## >> Sample from t-distribution ----
 ## CSwR 4-2-1
 {
   n <- 10000
@@ -74,8 +76,8 @@ par(mfrow=c(1,1))
 {
   set.seed(42)
   p <- 0.5
-  mu1 <- -4
-  mu2 <- 4
+  mu1 <- -10
+  mu2 <- 10
   sigma1 <- 1
   sigma2 <- 2
   nu <- 5
@@ -104,43 +106,188 @@ par(mfrow=c(1,1))
 
 
 
+## ESTIMATION ====
 
-
+## > Simulate data ----
 
 {
-  #par <- runif(5, 0.1, 1)
-  par <- c(runif(1, 0.1, 0.9), runif(1, -0.9, 0.9), runif(1, -0.9, 0.9), runif(1, 0.5, 2.0), runif(1, 0.5, 2.0))
-  EM_tracer = tracer(c('sum_sq_diff'), N = 1)
+  set.seed(3630)
+  true_params <- c(0.3, -4, 5, 1, 3)
+  p <- true_params[1]
+  mu1 <- true_params[2]
+  mu2 <- true_params[3]
+  sigma1 <- true_params[4]
+  sigma2 <- true_params[5]
+  nu <- 5
+  n <- 5000
+  z <- sample(c(TRUE, FALSE), n, replace = TRUE, prob = c(p, 1 - p))
+  ## Conditional simulation from the mixture components
+  x_sim <- numeric(n)
+  n1 <- sum(z)
+  x_sim[z] <- rt(n1, df=5)*sigma1 + mu1 #rnorm(n1, mu1, sigma1)
+  x_sim[!z] <- rt(n - n1, df=5)*sigma2 + mu2 #rnorm(n - n1, mu2, sigma2)
+}
+
+## Estimate ----
+{
+  set.seed(3630)
+  
+  ## Initial params: mu1 and mu2 should have same signs as real params (eg. guess from density plot) for convergence
+  par <- c(runif(1, 0.1, 0.9), runif(1, -0.9, -0.1), runif(1, 0.1, 0.9), runif(1, 0.5, 2.0), runif(1, 0.5, 2.0))
+  
+  ## Tracer
+  #EM_tracer = tracer(c("sum_sq_diff", "H_value", "h_prime", "gamma"), N = 1)
+  #EM_tracer = tracer(c("sum_sq_diff"), N = 1)
+  #EM_tracer = tracer(c("H_value"), N = 1)
+  #EM_tracer = tracer(c("h_prime"), N = 1)
+  #EM_tracer = tracer(c("grad_H"), N = 1)
+  #EM_tracer = tracer(c("par1"), N = 1)
+  EM_tracer = tracer(c("par", "sum_sq_diff", "H_value"), N = 1)
+  
   estimate <- EM(
     par,
-    y,
-    d = 0.8, 
-    c = 0.1, 
-    gamma0 = 1e-3,
-    maxit = 10000,
-    epsilon = 1e-12,
+    x_sim,
+    d = 0.7, 
+    c = 0.3, 
+    gamma0 = 0.01,
+    nu = 5, 
+    maxit = 1000,
+    maxit_gd = 1, ## No need to optimize too much in GD. Even one step is enough.
+    maxit_range_check = 1000, ## Drive p into range if needed
+    epsilon = 1e-10,
+    optimizer = "gd",
+    eps_gd = 1e-16,
     #cb = NULL
     cb = EM_tracer$trace
   )
-  estimate
-  #print(par)
   #summary(EM_tracer)
+  #head(summary(EM_tracer))
+  #tail(summary(EM_tracer))
+  print("Estimate: "); estimate
+  print("True params: "); true_params
+  print("Error: "); true_params - estimate
 }
 
-
-EM_trace <- summary(EM_tracer)
-# EM_trace <- transform(
-#   EM_trace,
-#   n = 1:nrow(EM_trace),
-#   par_norm_diff = sqrt((par0.1 - par.1)^2 + (par0.2 - par.2)^2)
-# )
-qplot(1:nrow(EM_trace), sum_sq_diff, data = EM_trace)
-
-
-## Plot estimated distribution
+## > Plot estimated distribution ----
 {
-  hist(y, prob=TRUE, breaks=40)
-  rug(y)
+  hist(x_sim, prob=TRUE, breaks=40)
+  rug(x_sim)
   x <- seq(min(y), max(y), length.out = 200)
   curve(f_h(x, estimate[4], estimate[5], estimate[2], estimate[3], estimate[1], nu), add = TRUE, col = "red")
 }
+
+## > Plot convergence ----
+
+bind_rows(tracer = summary(EM_tracer)) %>% 
+  autoplot(y = sum_sq_diff)
+
+bind_rows(tracer = summary(EM_tracer)) %>% 
+  autoplot(y = H_value)
+
+bind_rows(tracer = summary(EM_tracer)) %>% 
+  autoplot(y = H_value - min(H_value, na.rm = TRUE)) + scale_y_log10()
+
+## same
+# ggplot(summary(EM_tracer), aes(.time, H_value - min(H_value, na.rm = TRUE))) +
+#   geom_point() + 
+#   geom_line() + 
+#   scale_y_log10()
+
+
+## > Contour ----
+## Note: The fixed parameter values are the theoretical values, not the empirical!
+
+## >> Contour wrt p, mu1 ----
+{
+  mu2 <- true_params[3]
+  sigma1 <- true_params[4]
+  sigma2 <- true_params[5]
+  x_contour = seq(0.0001, 0.999, length.out = 101)
+  y_contour =  seq(-10, 10, length.out = 101)
+  ## Define function to compute in each grid point
+  ll_contour <- function(x, y) {
+    negll(c(x, y, mu2, sigma1, sigma2), x_sim)
+  }
+  z_contour <- outer(x_contour, y_contour, Vectorize(ll_contour))
+  
+  fig3 <- plot_ly(
+    x = x_contour, 
+    y = y_contour, 
+    z = t(z_contour), ## z matrix must be transposed for correct contour plot! (Don't know why)
+    type = "contour",
+    contours = list(
+      colorscale='heatmap',
+      showlabels = TRUE),
+    ncontours = 30
+  )
+  x_label <- list(
+    title = "p"
+  )
+  y_label <- list(
+    title = "mu 1"
+  )
+  fig3 <- fig3 %>% 
+    add_trace(type = "scatter", x = summary(EM_tracer)$par.1, y = summary(EM_tracer)$par.2, mode = "lines+markers") %>% 
+    layout(xaxis = x_label, yaxis = y_label) %>% 
+    layout(title=paste("mu2 = ", mu2, ", sigma1 = ", sigma1, ", sigma2 = ", sigma2))
+  fig3
+}
+
+
+## >> Contour wrt sigma1, sigma2 ----
+## Why does the algorithm move away from the minimum at first?
+## The other params decreas sufficiently to drive the square d difference down.
+
+{
+  p <- true_params[1]
+  mu1 <- true_params[2]
+  mu2 <- true_params[3]
+  x_contour = seq(0.1, 6.1, length.out = 101)
+  y_contour =  seq(0.1, 6.1, length.out = 101)
+  ## Define function to compute in each grid point
+  ll_contour <- function(x, y) {
+    negll(c(p, mu1, mu2, x, y), x_sim)
+  }
+  z_contour <- outer(x_contour, y_contour, Vectorize(ll_contour))
+  
+  fig4 <- plot_ly(
+    x = x_contour, 
+    y = y_contour, 
+    z = t(z_contour), ## z matrix must be transposed for correct contour plot! (Don't know why)
+    type = "contour",
+    contours = list(
+      showlabels = TRUE),
+    ncontours = 300
+  )
+  x_label <- list(
+    title = "sigma 1"
+  )
+  y_label <- list(
+    title = "sigma 2"
+  )
+  fig4 <- fig4 %>% 
+    add_trace(x = summary(EM_tracer)$par.4, y = summary(EM_tracer)$par.5, type = "scatter", mode = "lines+markers") %>% 
+    layout(xaxis = x_label, yaxis = y_label) %>% 
+    layout(title=paste("p2 = ", p, ", mu1 = ", mu1, ", mu2 = ", mu2))
+  fig4
+}
+
+## z matrix must be transposed for correct contour plot! (Don't know why)
+## Test:
+{
+  xtmp <- 1:5
+  ytmp <- 1:5 * 10
+  ztmp <- outer(xtmp, ytmp, function(x,y) {y - x})
+  
+  ## The contour line should be y - x, like here:
+  plot_ly(
+    x = xtmp, 
+    y = ytmp, 
+    z = t(ztmp), ## z matrix must be transposed for correct contour plot! (Don't know why)
+    type = "contour",
+    contours = list(
+      showlabels = TRUE)
+  )
+}
+
+
